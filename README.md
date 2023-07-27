50dd1f69c2477f1175a35be055248de4abc8714c
Ruben Arcos <ruben.arcos06@gmail.com>
2023-07-26 21:01:10 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # addressBook
+
+Whiteboard your app
+
+Make a code plan
+
+Pseudo code and then translate to JavaScript
+Create a new repo (name it descriptively well), clone it to your local machine and share it with your partner
+
+Open the directory and create an index.html and main.js file to fetch a user and display them one at at time
+
+Use the API: https://randomuser.me/api/ to fetch a user now
+Fetch a new user multiple times and store them in an array
+
+    function to perform a basic fetch and put the results in the addressBook array
+
+then list out all the users in your address book array by name and picture
+    
+loop through ... use .map()
+
+figure out how to to fetch multiple users in one fetch request
+
+    look at docs:
+
+    https://randomuser.me/api/?results=20&nat=us
+
+Then list out all the users in your address book array by name and picture
+Figure out how to fetch multiple users in one fetch request
+
+Fetch multiple users on window load
+    run the fetch function as soon as the web page loads in the window
+
+    window.onloadI(call the mainFunction()) first line in my js file
+
+Add a button to each user that when clicked displays the rest of their information like DOB, address and so forth
+
+    toggle button  show or hide the extra info
+
+    https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+
+Once you have the functionality working, feel free to style and structure your address book with CSS and HTML
---
+++ main.js
@@ -0,0 +1,323 @@
+let addressBook = [
+{
+"gender": "female",
+"name": {
+"title": "Mrs",
+"first": "Rosina",
+"last": "Litvinovich"
+},
+"location": {
+"street": {
+"number": 5889,
+"name": "Mikoli Ievshana"
+},
+"city": "Nemiriv",
+"state": "Kirovogradska",
+"country": "Ukraine",
+"postcode": 74246,
+"coordinates": {
+"latitude": "75.2923",
+"longitude": "108.1102"
+},
+"timezone": {
+"offset": "-9:00",
+"description": "Alaska"
+}
+},
+"email": "rosina.litvinovich@example.com",
+"login": {
+"uuid": "b8819396-b11c-494c-bd1a-a593e4b66bb7",
+"username": "redmouse407",
+"password": "barney",
+"salt": "hfXLrW7Z",
+"md5": "b0fc71a6fe4029510c817de4f51bdf3c",
+"sha1": "431391f66579ca55e25b74873af886e5d91ef54e",
+"sha256": "d8abe67d3daaec31f55b69566e895e00d47115c7e9d8534a3295cd0e54d58a92"
+},
+"dob": {
+"date": "1977-07-14T07:39:05.089Z",
+"age": 46
+},
+"registered": {
+"date": "2006-12-28T06:50:28.162Z",
+"age": 16
+},
+"phone": "(068) R43-6749",
+"cell": "(097) P14-8600",
+"id": {
+"name": "",
+"value": null
+},
+"picture": {
+"large": "https://randomuser.me/api/portraits/women/90.jpg",
+"medium": "https://randomuser.me/api/portraits/med/women/90.jpg",
+"thumbnail": "https://randomuser.me/api/portraits/thumb/women/90.jpg"
+},
+"nat": "UA"
+},
+
+{
+"gender": "male",
+"name": {
+"title": "Mr",
+"first": "Arthur",
+"last": "Chen"
+},
+"location": {
+"street": {
+"number": 519,
+"name": "Prebensen Drive"
+},
+"city": "Timaru",
+"state": "Marlborough",
+"country": "New Zealand",
+"postcode": 60664,
+"coordinates": {
+"latitude": "47.1905",
+"longitude": "-87.1198"
+},
+"timezone": {
+"offset": "+4:30",
+"description": "Kabul"
+}
+},
+"email": "arthur.chen@example.com",
+"login": {
+"uuid": "10a0ad69-4762-4660-82ea-243e03516835",
+"username": "tinymouse232",
+"password": "beetle",
+"salt": "pEzjxxcr",
+"md5": "e75dd419d679db91f3d1fd951aeee8a2",
+"sha1": "56886ce5987a38b0764fdd34c07947e8b086c5e7",
+"sha256": "0a2ce1bc7d7232a66d8baec1e6ec44441219cb4e4ba5d242804f0a0283ecf0bb"
+},
+"dob": {
+"date": "1959-10-14T11:28:53.245Z",
+"age": 63
+},
+"registered": {
+"date": "2006-01-28T15:27:16.164Z",
+"age": 17
+},
+"phone": "(023)-135-2345",
+"cell": "(773)-945-3481",
+"id": {
+"name": "",
+"value": null
+},
+"picture": {
+"large": "https://randomuser.me/api/portraits/men/90.jpg",
+"medium": "https://randomuser.me/api/portraits/med/men/90.jpg",
+"thumbnail": "https://randomuser.me/api/portraits/thumb/men/90.jpg"
+},
+"nat": "NZ"
+},
+
+{
+"gender": "male",
+"name": {
+"title": "Monsieur",
+"first": "Miro",
+"last": "Rey"
+},
+"location": {
+"street": {
+"number": 1242,
+"name": "Rue Jean-Baldassini"
+},
+"city": "Kehrsatz",
+"state": "Zürich",
+"country": "Switzerland",
+"postcode": 2643,
+"coordinates": {
+"latitude": "-62.8756",
+"longitude": "-25.7974"
+},
+"timezone": {
+"offset": "+5:30",
+"description": "Bombay, Calcutta, Madras, New Delhi"
+}
+},
+"email": "miro.rey@example.com",
+"login": {
+"uuid": "bdb1e6ff-dcf2-4a60-8577-2fde254e444b",
+"username": "heavykoala215",
+"password": "francis",
+"salt": "3W0iQAbr",
+"md5": "1356ce1d36fbbf12852aa35edc9a148d",
+"sha1": "4fc28f141ae993a803cc87c505383491624afffd",
+"sha256": "380185853c0785bbaf0f7edf207abe0f11beef05f578539631007635dd87fd52"
+},
+"dob": {
+"date": "2000-03-14T04:10:42.185Z",
+"age": 23
+},
+"registered": {
+"date": "2019-01-15T11:24:43.720Z",
+"age": 4
+},
+"phone": "076 392 22 73",
+"cell": "077 351 26 26",
+"id": {
+"name": "AVS",
+"value": "756.6907.7606.69"
+},
+"picture": {
+"large": "https://randomuser.me/api/portraits/men/91.jpg",
+"medium": "https://randomuser.me/api/portraits/med/men/91.jpg",
+"thumbnail": "https://randomuser.me/api/portraits/thumb/men/91.jpg"
+},
+"nat": "CH"
+},
+
+{
+"gender": "male",
+"name": {
+"title": "Mr",
+"first": "Vidaúl",
+"last": "Pires"
+},
+"location": {
+"street": {
+"number": 7380,
+"name": "Rua Vinte E Um"
+},
+"city": "Parnaíba",
+"state": "Bahia",
+"country": "Brazil",
+"postcode": 75059,
+"coordinates": {
+"latitude": "-80.4558",
+"longitude": "-52.2057"
+},
+"timezone": {
+"offset": "+4:30",
+"description": "Kabul"
+}
+},
+"email": "vidaul.pires@example.com",
+"login": {
+"uuid": "fcabfa2b-38ed-49f8-801d-a9c2c380441f",
+"username": "bigswan714",
+"password": "4121",
+"salt": "lI33M7FI",
+"md5": "40eab85fe1fe4eeb4d86078790c8f487",
+"sha1": "3ed58e829a868e6f0413aeab3ed547950276ca50",
+"sha256": "e844eadabe5b77c85dd77860de6bf312a00dc36cf92fdbc1ba31b27a970cb174"
+},
+"dob": {
+"date": "1979-01-04T13:45:49.143Z",
+"age": 44
+},
+"registered": {
+"date": "2018-02-12T17:21:58.234Z",
+"age": 5
+},
+"phone": "(82) 5023-0941",
+"cell": "(47) 2327-5405",
+"id": {
+"name": "CPF",
+"value": "924.840.767-53"
+},
+"picture": {
+"large": "https://randomuser.me/api/portraits/men/16.jpg",
+"medium": "https://randomuser.me/api/portraits/med/men/16.jpg",
+"thumbnail": "https://randomuser.me/api/portraits/thumb/men/16.jpg"
+},
+"nat": "BR"
+},
+
+{
+"gender": "female",
+"name": {
+"title": "Mrs",
+"first": "Edna",
+"last": "Holland"
+},
+"location": {
+"street": {
+"number": 7706,
+"name": "Valwood Pkwy"
+},
+"city": "Warrnambool",
+"state": "Tasmania",
+"country": "Australia",
+"postcode": 9674,
+"coordinates": {
+"latitude": "-89.4436",
+"longitude": "57.9309"
+},
+"timezone": {
+"offset": "+1:00",
+"description": "Brussels, Copenhagen, Madrid, Paris"
+}
+},
+"email": "edna.holland@example.com",
+"login": {
+"uuid": "4667cdfb-3b6a-4fd2-ae8b-ba0bb70321d6",
+"username": "sadpanda686",
+"password": "kume",
+"salt": "RzP0ZkP3",
+"md5": "6f3af0c28adb46fa7700702c837c5ad4",
+"sha1": "c8a293473c7602bb73b7531dc582f6b035750cce",
+"sha256": "c48138206dc4c0996af215c00a69bde20042297a84657405f84218c22d5a397b"
+},
+"dob": {
+"date": "1946-04-03T06:21:50.804Z",
+"age": 77
+},
+"registered": {
+"date": "2017-03-08T06:43:03.473Z",
+"age": 6
+},
+"phone": "04-0210-8467",
+"cell": "0415-881-790",
+"id": {
+"name": "TFN",
+"value": "690267575"
+},
+"picture": {
+"large": "https://randomuser.me/api/portraits/women/70.jpg",
+"medium": "https://randomuser.me/api/portraits/med/women/70.jpg",
+"thumbnail": "https://randomuser.me/api/portraits/thumb/women/70.jpg"
+},
+"nat": "AU"
+}
+
+]
+
+const getContacts = () => {
+    fetch('https://randomuser.me/api/?results=20&nat=us')
+    .then((res) => res.json() )
+    .then(data => addressBook = data.results)
+    .then(console.log(addressBook))
+    .then(addressBook => addressBook.map((person) => {
+
+        let html = `
+
+        <img src="${person.picture.large}">
+        <h2>${person.name.first} ${person.name.last}</h2>
+        <div id="moreInfo">
+            <h3>${person.address}</h3>
+            <h3>${person.email}</h3>
+            <h3>${person.phone}</h3>
+            <h3>${person.dob}</h3>   
+        </div>
+        <button> onclick=calls the toggle function>more/less info button that toggle visibility</button>
+
+
+
+
+        
+        
+        
+        `; //end of interp string
+    }))
+}
+
+
+// dark theme
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
